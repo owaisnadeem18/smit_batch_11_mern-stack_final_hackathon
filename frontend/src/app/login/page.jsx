2b402406f18bfd997c8d9748e07b80a5336d7b8d
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-01-24 19:46:11 +0500

    did more changes

---
+++ frontend/src/app/login/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LogIn = () => {
+  return (
+    <div className='min-h-screen text-white bg-slate-800' >
+      <h2 className='text-3xl' >
+        I am Login Component
+      </h2>
+
+      
+
+    </div>
+  )
+}
+
+export default LogIn
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,20 @@
+"use client";
+
+import Link from "next/link";
 
 export default function Home() {
+
   return (
     <>
-      <h2 className="bg-slate-900 p-4 text-4xl text-white" >
-        Final Grand Hackathon of SMIT
-      </h2>
+      <div className="flex bg-slate-900 text-white items-center justify-between p-4">
+        <h2 className="text-2xl">Final Grand Hackathon of SMIT</h2>
+        <Link
+          href={"/login"}  
+          className="text-xl py-3 px-6 bg-slate-700 rounded-xl"
+        >
+          LogIn
+        </Link>
+      </div>
     </>
   );
 }
