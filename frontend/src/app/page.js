cdbfdb08a568826678d52f519b3041e4fc99e2ca
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-01-24 15:30:31 +0500

    started my next js project

---
+++ frontend/src/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+      <h2 className="bg-slate-900 p-4 text-4xl text-white" >
+        Final Grand Hackathon of SMIT
+      </h2>
+    </>
+  );
+}
